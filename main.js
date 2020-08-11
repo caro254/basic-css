0b62d59110fe3db3331f4a531c791777781a3545
Caroline .A. Maunda <54573902+caro254@users.noreply.github.com>
2020-08-11 09:22:51 +0300

    changes in main.js style.css and index.html

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
+function changeColor(){
+ var text =document.getElementById("para").style.color="yellow";  
+}
 
+function backToNormal(){
+    var text =document.getElementById("para").style.color=""; 
+}
     
