d4762e9e5c903f9913f7bf4a0bf7ded1f3bb6830
caroline <paulomugoo>
2020-09-27 20:30:58 +0300

    changes in index html and styl.css

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+
+
+
+
+
 function changeColor(){
  var text =document.getElementById("para").style.color="yellow";  
 }
@@ -7,3 +12,33 @@ function backToNormal(){
 }
     
 
+
+function changeColor(){
+var text = document.getElementById("blocks").style.color="red";
+}
+function backToNormal(){
+var text = document.getElementById("blocks").style.color ="";
+}
+
+var a = document.createElement('a');
+var linkText = document.createTextNode("About mario");
+a.appendChild(linkText);
+a.title = "my title text";
+a.href = "History.html";
+document.body.appendChild(a);
+document.body.prepend(a);
+
+let sub = document.querySelector(".submit");
+sub.style.backgroundColor = "Black";
+sub.style.color = "white";
+sub.style.fontSize = "3em";
+
+var btn = document.createElement("BUTTON");   // Create a <button> element
+btn.innerHTML = "CLICK ME";                   // Insert text
+document.body.appendChild(btn);
+
+
+const text = document.querySelector(".title");
+const changeColor= document.querySelector(".changeColor");
+
+text.style.backgroundColor= "white";
